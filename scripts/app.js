b9b9fca75bde8087ab760214ed09ca941b41ea2f
captain-zhu <andypaker@qq.com>
2016-09-15 20:26:48 +0800

    fix bugs

--- scripts/app.js
+++ scripts/app.js
@@ -1,56 +1,49 @@
 (function () {
-    angular.module('LunchCheck', [])
-    
+  angular.module('LunchCheck', [])
+
     .controller('LunchCheckController', LunchCheck);
-    
-    LunchCheck.$inject = ['$scope'];
-    
-     function LunchCheck($scope) {
-        $scope.lunch = {};
-        $scope.message = "";
-        $scope.checkIfTooMuch = check;
-         $scope.fontColor = "green";
-         $scope.borderColor = "green";
-         
-        
-        function check() {
-            if ($scope.lunch.count === undefined) {
-                errorMsg();
-                return;
-            }
-            var trimedCount = $scope.lunch.count.trim()
-            var count = parseInt(trimedCount);
-            if (isNaN(trimedCount) || isNaN(count)) {
-                errorMsg();
-                return;
-            }
-        
-            if (count >= 0 && count < 4) {
-                enjoyMsg();
-            } else if (count >= 4) {
-                tooMuchMsg();
-            } else {
-                errorMsg();
-            }
-        };
-            
-            function errorMsg() {
-                $scope.message = 'Please enter data first';
-                $scope.fontColor = "red";
-                $scope.borderColor = "red";
-            };
-         
-         function enjoyMsg() {
-             $scope.message = 'Enjoy!';
-                $scope.fontColor = "green";
-                $scope.borderColor = "green";
-         };
-         
-         function tooMuchMsg() {
-             $scope.message = 'Too much!';
-                $scope.fontColor = "green";
-                $scope.borderColor = "green";
-         };
-     }
-         
+
+  LunchCheck.$inject = ['$scope'];
+
+  function LunchCheck($scope) {
+    $scope.lunch = {};
+    $scope.message = "";
+    $scope.checkIfTooMuch = check;
+    $scope.fontColor = "green";
+    $scope.borderColor = "green";
+
+
+    function check() {
+      if ($scope.lunch.content === undefined) {
+        errorMsg();
+        return;
+      }
+      var lunchArray = $scope.lunch.content.split(',');
+      var length = lunchArray.length;
+      if (length >= 0 && length < 4) {
+        enjoyMsg();
+      } else if (length >= 4) {
+        tooMuchMsg();
+      }
+    };
+
+    function errorMsg() {
+      $scope.message = 'Please enter data first';
+      $scope.fontColor = "red";
+      $scope.borderColor = "red";
+    };
+
+    function enjoyMsg() {
+      $scope.message = 'Enjoy!';
+      $scope.fontColor = "green";
+      $scope.borderColor = "green";
+    };
+
+    function tooMuchMsg() {
+      $scope.message = 'Too much!';
+      $scope.fontColor = "green";
+      $scope.borderColor = "green";
+    };
+  }
+
 })();
